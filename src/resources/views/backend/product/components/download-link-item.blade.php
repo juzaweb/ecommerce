ec357c8a4eb85356e942d64aa83a07694587c4aa
Juzaweb <dangtheanh16@gmail.com>
2024-01-27 12:47:21 +0700

    :+1: Support downloadable

---
+++ src/resources/views/backend/product/components/download-link-item.blade.php
@@ -0,0 +1,21 @@
+<tr>
+    <td>
+        <input type="text"
+               name="download_links[{{ $marker }}][name]"
+               class="form-control"
+               value="{{ $item->name ?? '' }}"
+        />
+    </td>
+    <td>
+        {{ Field::uploadUrl(
+            trans('File Url'),
+            "download_links[{$marker}][url]",
+            [
+                'placeholder' => 'https://',
+                'show_label' => false,
+                'id' => "download-links-{$marker}",
+                'disk' => 'protected'
+            ]
+        ) }}
+    </td>
+</tr>
--- src/resources/views/backend/product/form.blade.php
+++ src/resources/views/backend/product/form.blade.php
@@ -1,4 +1,15 @@
+@php
+    /**
+     * @var \Juzaweb\Backend\Models\Post $model
+     */
+@endphp
+
 <div class="card">
+    <div class="card-header">
+        <div class="d-flex flex-column justify-content-center">
+            <h5 class="mb-0">{{ __('Product Info') }}</h5>
+        </div>
+    </div>
     <div class="card-body">
         <div class="row">
             <div class="col-md-6">
@@ -59,7 +70,49 @@
     </div>
 </div>
 
+<div class="card">
+    <div class="card-header">
+        <div class="d-flex flex-column justify-content-center">
+            <h5 class="mb-0">{{ __('Advanced') }}</h5>
+        </div>
+    </div>
+
+    <div class="card-body">
+        {{ Field::checkbox(trans('Downloadable'), 'meta[downloadable]', ['checked' => $model->getMeta('downloadable') == 1]) }}
+
+        <div class="row download-links-box form-repeater">
+
+            <div class="col-md-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="w-25">{{ trans('File Name') }}</th>
+                            <th>{{ trans('File Url') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody class="repeater-items">
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col-md-12">
+                <button type="button" class="btn btn-primary btn-sm add-repeater-item">
+                    {{ trans('cms::app.add_repeater_item', ['label' => trans('Download Links')]) }}
+                </button>
+            </div>
+
+            <script type="text/html" class="repeater-item-template">
+                @component('ecom::backend.product.components.download-link-item', ['marker' => '{marker}'])
+
+                @endcomponent
+            </script>
+        </div>
+
+    </div>
+</div>
+
 {{ Field::images(trans('ecom::content.images'), 'meta[images]', [
-    'value' => $variant->images ?? [],
+    'value' => $model->getMeta('images', []),
 ]) }}
 
